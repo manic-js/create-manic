797fe02eb2b963b7615bd3045025fcf6c4c69516
Rahul Marban <rahulmarban@gmail.com>
2026-04-17 10:47:29 +0530

    chore: updated create manic tempalte

--- template/manic.config.ts
+++ template/manic.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'manicjs/config';
+import { apiDocs } from '@manicjs/api-docs';
 
 export default defineConfig({
   app: {
     name: 'Manic',
   },
-
   server: {
     port: 6070,
   },
+  plugins: [apiDocs()],
 });
